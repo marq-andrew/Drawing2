9233c2c029edb9acc1d9ffbece28e84c972fed1c
Mark Andrew <mchugh@ozemail.com.au>
2019-01-14 17:03:23 +1100

    Initial Commit

---
+++ Drawing2/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Drawing2
+//
+//  Created by Mark McHugh on 14/1/19.
+//  Copyright © 2019 Mark Andrew. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
